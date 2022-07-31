e9236444179cdbef643e03431e2ff8e12e8a98bd
Chris Phillips <christopher.trevor.phillips@gmail.com>
2022-07-30 22:53:11 -0700

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,6 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-// messageOne.textContent = 'From Javascript';
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -12,17 +10,12 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
     
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch(' /weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error){
-                // console.log(data.error);
-
                 messageOne.textContent = data.error;
             }
             else{
-                // console.log(data.location);
-                // console.log(data.forecast);
-
                 messageOne.textContent = data.location;
                 messageTwo.textContent = data.forecast;
             }
